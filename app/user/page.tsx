5d51ecf86009994983ae0a380687c44303b9ea5e
Narayan-21 <naaidjan.19@gmail.com>
2024-08-28 00:18:10 +0530

    server component

---
+++ app/user/page.tsx
@@ -0,0 +1,9 @@
+import { getServerSession } from "next-auth"
+
+export default async function () {
+    const session = await getServerSession();
+    return <div>
+        User Component
+        {JSON.stringify(session)}
+    </div>
+}
